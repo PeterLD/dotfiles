9402da5b61d32e6ccb0c59360f32c1fbc7506eff
Peter Lyle-Dugas <p.lyled@gmail.com>
2020-06-03 22:22:12 -0700

    Add global gitignore

--- install.sh
+++ install.sh
@@ -1,3 +1,9 @@
 #!/usr/bin/env bash
+
+# Vim
 mkdir $HOME/.vim
 ln -s $(pwd)/vimrc $HOME/.vim/vimrc
+
+# Git
+ln -s $(pwd)/gitignore $HOME/.gitignore
+git config --global core.excludesFile $HOME/.gitignore
